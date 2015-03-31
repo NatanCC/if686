a00855254fa700130057ec081dba6b539a985889
unknown <nss@G1C36.windows.cin.ufpe.br>
2015-03-31 09:31:01 -0300

    lookAndSay terminada

--- 20150331.hs
+++ 20150331.hs
@@ -12,11 +12,26 @@
 
 -- Questão 2
 
+lookAndSay :: Int -> Int
+lookAndSay 0 = 0
+lookAndSay 1 = 1
+lookAndSay x = listToInt sequencia (reverseList(count2 sequencia 1))
+            where
+			listaSequencia = lookAndSay3 [1] (x-1)
+			sequencia = head(reverseList listaSequencia)
+			
 
-lookAndSay :: [Int] -> [Int]
-lookAndSay [0] = []
-lookAndSay (x:xs) = say((push x (x:xs))) ++ (lookAndSay (pop x (x:xs)))
-lookAndSay [] = []
+
+lookAndSay2 :: [Int] -> [Int]
+lookAndSay2 [0] = []
+lookAndSay2 (x:xs) = say((push x (x:xs))) ++ (lookAndSay2 (pop x (x:xs)))
+lookAndSay2 [] = []
+
+lookAndSay3 :: [Int] -> Int -> [[Int]]
+lookAndSay3 _ 0 = []
+lookAndSay3 x y = [w] ++ lookAndSay3 w (y-1)
+                where
+				w = lookAndSay2 x
 
 push :: Int -> [Int] -> [Int]
 push _ [] = []
@@ -37,3 +52,19 @@ count (x:xs) = 1 + count xs
 say :: [Int] -> [Int]
 say [] = []
 say (x:xs) = [(count (x:xs)), x]
+
+-- Funções para transformar a list de sequencia em um Int
+
+count2 :: [Int] -> Int -> [Int]
+count2 [] _ = []
+count2 (x:xs) y = (y: (count2 xs (y*10)))
+
+reverseList [] = []
+reverseList (x:xs) = reverseList xs ++ [x]
+
+listToInt :: [Int] -> [Int] -> Int
+listToInt [] _ = 0
+listToInt _ [] = 0
+listToInt (x:xs) (y:ys) = (x*y) + listToInt xs ys
+
+
