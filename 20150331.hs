24ef336b9239d6070f46bc6f5a0d205c45082f4c
unknown <nss@G1C36.windows.cin.ufpe.br>
2015-03-31 08:30:04 -0300

    ideia base de look and say

--- 20150331.hs
+++ 20150331.hs
@@ -11,3 +11,29 @@
 -}
 
 -- Questão 2
+
+
+lookAndSay :: [Int] -> [Int]
+lookAndSay [0] = []
+lookAndSay (x:xs) = say((push x (x:xs))) ++ (lookAndSay (pop x (x:xs)))
+lookAndSay [] = []
+
+push :: Int -> [Int] -> [Int]
+push _ [] = []
+push x (y:ys)
+    | x == y = (y:(push x ys))
+	| otherwise = []
+	
+pop :: Int -> [Int] -> [Int]
+pop _ [] = []
+pop x (y:ys)
+    | x == y = (pop x ys)
+	| otherwise = (y:ys)
+	
+count :: [Int] -> Int
+count [] = 0
+count (x:xs) = 1 + count xs
+
+say :: [Int] -> [Int]
+say [] = []
+say (x:xs) = [(count (x:xs)), x]
