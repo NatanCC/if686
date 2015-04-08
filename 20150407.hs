173f1393a4915255644800c4eeb63807e06e6717
Natanael Souza <nss@cin.ufpe.br>
2015-04-08 19:27:42 -0300

    Adicionado os exercícios que não deram tempo de fazer na aula

--- 20150407.hs
+++ 20150407.hs
@@ -34,3 +34,53 @@ plcIn [] _ = False
 plcIn (x:xs) s
     | x == s = True
     | otherwise = plcIn xs s
+
+data Expr = Lit Int
+    | Add Expr Expr
+    | Sub Expr Expr
+        deriving (Show)    
+
+data List t = Nil
+    | Cons t (List t)
+
+data Tree t = NilT
+    | Node t (Tree t) (Tree t)
+        deriving (Show, Eq)
+
+
+showExpr :: Expr -> String
+showExpr e = show e
+
+
+toList :: List t -> [t]
+toList Nil = []
+toList (Cons x y) = (x: toList y)
+
+lista :: List Int
+lista = Cons 5 (Cons 4 (Nil))
+
+fromList :: [t] -> List t
+fromList [] = Nil
+fromList (x:xs) = (Cons x (fromList xs))
+
+depth :: Tree t -> Int
+depth NilT = 0
+depth (Node x NilT NilT) = 0
+depth (Node x l r) = max (1 + depth l) (1 + depth r)
+
+collapse :: Tree t -> [t]
+collapse NilT = []
+collapse (Node x l r) = [x]++ collapse l ++ collapse r
+
+tree ::Tree Int
+tree = (Node 43 (Node 32 (Node 5 (NilT) (Node 2 (NilT) (NilT))) (NilT)) (Node 1 (NilT) (Node 98 (NilT) (NilT))))
+
+bfs :: (Eq t) => Tree t -> t -> Bool
+bfs NilT _ = False
+bfs (Node n l r) x
+    | n == x = True
+    | otherwise = False || bfs l x|| bfs r x
+
+mapTree :: (t -> u) -> Tree t -> Tree u
+mapTree _ NilT = NilT
+mapTree f (Node n l r) = (Node (f n) (mapTree f l) (mapTree f r))
