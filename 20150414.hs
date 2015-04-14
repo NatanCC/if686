eaa99309c2aa80139fdfa15b1e0bf217629b074b
Natanael Souza <nss@cin.ufpe.br>
2015-04-14 09:45:22 -0300

    questão 3 adicionada, porém bugada =C:/Program Files (x86)/Git

--- 20150414.hs
+++ 20150414.hs
@@ -10,30 +10,38 @@ compose f l = map comp l
 -- Questão 2
 
 data Graph t = NilG
-    | Node t [(t, Int)] (Graph t)
+    | NodeG t [(t, Int)] (Graph t)
         deriving (Show, Eq)
 
 mapGraph :: (a -> b) -> Graph a -> Graph b
 mapGraph _ NilG = NilG
-mapGraph f (Node n adjL g) = ((Node (f n)) (mapDupList f adjL) (mapGraph f g))
+mapGraph f (NodeG n adjL g) = ((NodeG (f n)) (mapDupList f adjL) (mapGraph f g))
 
 mapDupList :: (a -> b) -> [(a, Int)] -> [(b, Int)]
 mapDupList _ [] = []
 mapDupList f ((v, n):xs) = (((f v), n): mapDupList f xs)
 
 grafo :: Graph Int
-grafo = (Node 1 [(2, 2), (3, 2)] (Node 2 [(3, 1)] (Node 3 [] (NilG))))
+grafo = (NodeG 1 [(2, 2), (3, 2)] (NodeG 2 [(3, 1)] (NodeG 3 [] (NilG))))
 
 foldGraph :: (Num a) => (a -> a -> a) -> Graph a -> a
 foldGraph _ NilG = 0
-foldGraph f (Node v l g) = f v (foldGraph f 	g)
+foldGraph f (NodeG v l g) = f v (foldGraph f 	g)
 
 -- Questão 3
-
 data Tree t = NilT
     | Node t (Tree t) (Tree t)
         deriving (Show, Eq)
 
-filterTree :: (a -> Bool) -> Tree a -> [Tree a]
-filterTree _ NilT = [NilT]
 
+filterTree :: (Eq a) => (a -> Bool) -> Tree a -> [Tree a]
+filterTree _ NilT = [NilT]
+filterTree f (Node n tl tr)
+    | f n = [filterT f (Node n tl tr)] ++ filterTree f tl ++ filterTree f tr
+    | otherwise = [filterT f tl] ++ [filterT f tr]
+
+filterT :: (a -> Bool)  -> Tree a -> Tree a
+filterT _ NilT = NilT
+filterT f (Node n tl tr)
+    | f n = (Node n (filterT f tl) (filterT f tr))
+    | otherwise = NilT
